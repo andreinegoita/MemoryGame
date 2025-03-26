6111b4679080fda69a5591d90227a34fdad6735f
Negoita Andrei <lilbabyfan04@gmail.com>
2025-03-26 12:24:27 +0200

    Now the game when you prees start switches to startup game menu

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MemoryGame.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MemoryGame.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand StartCommand { get; }
+
+        public MainViewModel()
+        {
+            CurrentView = new View.MainMenuView();
+
+            StartCommand = new RelayCommand(SwitchToStartUpMenu);
+        }
+
+        private void SwitchToStartUpMenu(object obj)
+        {
+            CurrentView = new View.StartUpMenuView();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
